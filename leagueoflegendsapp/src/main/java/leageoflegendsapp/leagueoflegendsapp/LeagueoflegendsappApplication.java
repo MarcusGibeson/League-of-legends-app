cb42b3ea2279fb57fbbd7600d0d851929683ea69
Marcus Gibeson <133388744+MarcusGibeson@users.noreply.github.com>
2023-10-22 18:14:50 -0400

    first commit

---
+++ leagueoflegendsapp/src/main/java/leageoflegendsapp/leagueoflegendsapp/LeagueoflegendsappApplication.java
@@ -0,0 +1,13 @@
+package leageoflegendsapp.leagueoflegendsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueoflegendsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueoflegendsappApplication.class, args);
+	}
+
+}
---
+++ leagueoflegendsapp/src/test/java/leageoflegendsapp/leagueoflegendsapp/LeagueoflegendsappApplicationTests.java
@@ -0,0 +1,13 @@
+package leageoflegendsapp.leagueoflegendsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueoflegendsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
